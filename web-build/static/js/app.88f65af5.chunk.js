354f941c1ddab3c0161ebf5e56e6b35e279ecdbc
Yacoub Gadban <yacoub.gadban@gmail.com>
2021-11-23 23:18:58 +0200

    web build

---
+++ web-build/static/js/app.88f65af5.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonp=this.webpackJsonp||[]).push([[0],{38:function(e,t,n){"use strict";n.d(t,"a",(function(){return i}));var a=n(61),r=n(0),o=n.n(r),c=n(5),s=n(55),u=n(18);function i(){return o.a.createElement(u.a,{style:p.container},o.a.createElement(s.a,null,"Open up App.js to start working on your app!"),o.a.createElement(a.a,{style:"auto"}))}var p=c.a.create({container:{flex:1,backgroundColor:"#fff",alignItems:"center",justifyContent:"center"}})},62:function(e,t,n){e.exports=n(84)}},[[62,1,2]]]);
+//# sourceMappingURL=app.88f65af5.chunk.js.map
